410749a791ca31b603523cae782a4d42059579aa
Juan2Ramos <juan2ramos>
2016-05-22 17:15:17 -0500

    logs

---
+++ resources/views/errors/500.blade.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Be right back.</title>
+
+    <link rel="stylesheet" href="{{ asset('css/normalize.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <link href='https://fonts.googleapis.com/css?family=Roboto:400,300,100,500,700' rel='stylesheet' type='text/css'>
+
+
+</head>
+<body>
+
+<main>
+    <header class="Header">
+        <figure>
+            <img src="{{url('images/logo_ccb.jpg')}}" alt="Logo Camara de Comercio de Bogotá">
+        </figure>
+        <h1 class="Header-barInfo">
+            Bogotá Music Market - BOmm 2016 - Inscripciones
+        </h1>
+        <div class="Header-route">
+            <a href="http://www.bogotamusicmarket.com/">Inicio</a> <span> > </span> Información BOmm
+        </div>
+
+    </header>
+    <svg width="81px" height="47px" display="none" viewBox="0 0 81 47" version="1.1">
+        <g id="imageTemp" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+            <g id="Paso-2" transform="translate(-627.000000, -1870.000000)" fill="#C5C5C5">
+                <g id="imagenes" transform="translate(307.000000, 1823.000000)">
+                    <g id="Image" transform="translate(290.000000, 0.000000)">
+                        <path d="M110.525,93.2974915 L92.6972512,47 L70.253812,76.5502124 L55.2899396,61.775896 L30,93.2974915 L110.525,93.2974915 Z M40.0227774,49.0124537 C36.6850109,49.0124537 33.9854165,51.7231055 33.9854165,55.0750888 C33.9854165,58.4239127 36.6865905,61.1345646 40.0227774,61.1345646 C43.3573847,61.1345646 46.0585588,58.4239127 46.0585588,55.0750888 C46.0585588,51.7231055 43.3573847,49.0124537 40.0227774,49.0124537 L40.0227774,49.0124537 Z"
+                              id="Shape"></path>
+                    </g>
+                </g>
+            </g>
+        </g>
+    </svg>
+    <h2 class="title">error</h2>
+    <div class="container">
+        <div style="    margin: 100px 0;
+    text-align: center;
+    font-size: 40px;">
+            <div>¡Algo no salio bien!,  reporta este incidente a <a style="color: #be0f34;" href="mailto:artistas@bogotamusicmarket.com">artistas@bogotamusicmarket.com</a></div>
+        </div>
+    </div>
+
+    <footer class="Footer">
+        <hr>
+        <div class="Footer-info">
+            Contáctese con la CCB:<br>
+            Línea de Respuesta Inmediata (57 1) 3830330 |
+            <a href="http://www.ccb.org.co/Preguntas-frecuentes" target="_blank">Preguntas frecuentes</a> |
+            <a href="http://chat.millenium.com.co:8080/webchatccb/userinfo.jsp?chatID=N00A730aPr&workgroup=agentesccb@workgroup.chatminco.millenium.com.co"
+               target="_blank">Chat</a> |
+            <a href="http://190.144.149.101:8080/click/ccbclick.html" target="_blank">Llamada virtual </a> |
+            <a href="http://www.ccb.org.co/Escribanos" target="_blank">Contáctenos</a> |
+            <a href="http://sqyf.ccb.org.co/(S(ecrrfjf1wy1xde214cjzrxdq))/frmDefault.aspx#posicionpagina"
+               target="_blank"> Sugerencias, quejas y felicitaciones</a>
+            <a href="http://www.ccb.org.co/La-Camara-CCB/Nuestras-sedes" target="_blank">Mapa de sedes</a> |
+            <a href="http://www.ccb.org.co/La-Camara-CCB/Nuestras-sedes" target="_blank"> Directorio de sedes</a> |
+            <a href="http://www.ccb.org.co/La-Camara-CCB/Nuestras-sedes" target="_blank">Horarios de atención</a>
+        </div>
+        <hr>
+        <div>
+            Cámara de Comercio de Bogotá © Todos los derechos reservados | Términos y condiciones de uso de:
+            <a href="http://www.ccb.org.co/Terminos-y-condiciones" target="_blank">Sitio </a>,
+            <a href="http://chat.millenium.com.co:8080/webchatccb/userinfo.jsp?chatID=N00A730aPr&workgroup=agentesccb@workgroup.chatminco.millenium.com.co"
+               target="_blank">Chat</a> |y
+            <a href="http://www.ccb.org.co/Reglas-en-redes-sociales" target="_blank">Reglas en redes sociales</a> |
+            <a href="http://www.ccb.org.co/La-Camara-CCB/Nuestras-sedes" target="_blank">Mapa del sitio</a> |
+            <a href="http://www.ccb.org.co/Inscripciones-y-renovaciones/Medios-de-pago" target="_blank">Medios de
+                pago</a>
+            Recomendado visualizar en : IE7 - IE8 - Firefox 3.0 - Safari - Chrome | Este sitio está certificado por
+            <a href="https://web.certicamara.com/" target="_blank"> Certicámara S.A.</a>
+            Autoridad de certificación digital abierta | Optimizada 1024 x 768
+
+        </div>
+    </footer>
+</main>
+
+
+</html>
