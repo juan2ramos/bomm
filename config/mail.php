35cfb0665b431d29655db73ccdcf82db25a7bed4
Juan2Ramos <juan2ramos>
2016-05-22 17:45:31 -0500

    mail

--- config/mail.php
+++ config/mail.php
@@ -55,7 +55,7 @@ return [
     |
     */
 
-    'from' => ['address' => null, 'name' => null],
+    'from' => ['address' => 'artistas@bogotamusicmarket.com', 'name' => 'bogotamusicmarket'],
 
     /*
     |--------------------------------------------------------------------------
