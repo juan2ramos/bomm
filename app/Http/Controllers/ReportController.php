91622931f2b182105e3f385638795632ecbb2f5c
Juan2Ramos <juan2ramos>
2016-05-26 03:08:46 -0500

    excel

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -19,11 +19,11 @@ class ReportController extends Controller
 
         $groups = Group::whereHas('call', function ($query) {
             $query->whereRaw(' convocatoria = 2016  and id_grupos_musica > 31 ');
-        })->with(['call' => function($q){
+        })->with(['call' => function ($q) {
             $q->whereRaw(' convocatoria = 2016  and id_grupos_musica > 31 ');
         }])->orderBy('id', 'desc')->get();
 
-        $registers = Group::where('id', '>' ,'31')->count();
+        $registers = Group::where('id', '>', '31')->count();
         $finish = Call::whereRaw('convocatoria = 2016 and fecha_finalizacion IS NOT NULL and id_grupos_musica > 31')->count();
         $thirteen = Call::whereRaw('convocatoria = 2016 and inscripcion_inicial = 2013 and id_grupos_musica > 31 ')->count();
         $fourteen = Call::whereRaw('convocatoria = 2016 and inscripcion_inicial = 2014 and id_grupos_musica > 31 ')->count();
@@ -35,20 +35,76 @@ class ReportController extends Controller
         $fifteenFinish = Call::whereRaw('convocatoria = 2016 and inscripcion_inicial = 2015 and id_grupos_musica > 31 and fecha_finalizacion IS NOT NULL')->count();
         $sixteenFinish = Call::whereRaw('convocatoria = 2016 and inscripcion_inicial = 2016 and id_grupos_musica > 31 and fecha_finalizacion IS NOT NULL')->count();
 
-        return view('reportUsers', compact('groups', 'step','registers','finish',
-            'thirteen','fourteen','fifteen','sixteen', 'thirteenFinish','fourteenFinish','fifteenFinish','sixteenFinish'));
+        return view('reportUsers', compact('groups', 'step', 'registers', 'finish',
+            'thirteen', 'fourteen', 'fifteen', 'sixteen', 'thirteenFinish', 'fourteenFinish', 'fifteenFinish', 'sixteenFinish'));
     }
+
     public function usersExcel()
     {
         $date = Date::now()->format('l j F H:i:s');
-        Excel::create('reporte '.$date, function($excel) {
+        Excel::create('reporte ' . $date, function ($excel) {
             $groups = Group::whereHas('call', function ($query) {
                 $query->whereRaw('convocatoria = 2016');
-            })->where('id', '>' ,'31')->with('call')->get();
+            })->where('id', '>', '31')->with('call')->get();
 
-            $excel->sheet('reporte', function($sheet) use($groups) {
+            $excel->sheet('reporte', function ($sheet) use ($groups) {
+                $sheet->setCellValue('A2', 'Año');
+                $sheet->setCellValue('B2', 'Inscritos');
+                $sheet->setCellValue('C2', 'Finalizados');
+                $sheet->setCellValue('E2', 'Inscritos');
+                $sheet->setCellValue('E3', 'Inscritos finalizados');
 
-                $sheet->fromArray($groups);
+                $sheet->setCellValue('A2', '2013');
+                $sheet->setCellValue('A3', '2014');
+                $sheet->setCellValue('A4', '2014');
+                $sheet->setCellValue('A5', '2016');
+
+
+                $registers = Group::where('id', '>', '31')->count();
+                $finish = Call::whereRaw('convocatoria = 2016 and fecha_finalizacion IS NOT NULL and id_grupos_musica > 31')->count();
+                $thirteen = Call::whereRaw('convocatoria = 2016 and inscripcion_inicial = 2013 and id_grupos_musica > 31 ')->count();
+                $fourteen = Call::whereRaw('convocatoria = 2016 and inscripcion_inicial = 2014 and id_grupos_musica > 31 ')->count();
+                $fifteen = Call::whereRaw('convocatoria = 2016 and inscripcion_inicial = 2015 and id_grupos_musica > 31 ')->count();
+                $sixteen = Call::whereRaw('convocatoria = 2016 and inscripcion_inicial = 2016 and id_grupos_musica > 31 ')->count();
+
+                $thirteenFinish = Call::whereRaw('convocatoria = 2016 and inscripcion_inicial = 2013 and id_grupos_musica > 31 and fecha_finalizacion IS NOT NULL')->count();
+                $fourteenFinish = Call::whereRaw('convocatoria = 2016 and inscripcion_inicial = 2014 and id_grupos_musica > 31 and fecha_finalizacion IS NOT NULL')->count();
+                $fifteenFinish = Call::whereRaw('convocatoria = 2016 and inscripcion_inicial = 2015 and id_grupos_musica > 31 and fecha_finalizacion IS NOT NULL')->count();
+                $sixteenFinish = Call::whereRaw('convocatoria = 2016 and inscripcion_inicial = 2016 and id_grupos_musica > 31 and fecha_finalizacion IS NOT NULL')->count();
+
+
+                $sheet->setCellValue('B3', $thirteen);
+                $sheet->setCellValue('B4', $fourteen);
+                $sheet->setCellValue('B5', $fifteen);
+                $sheet->setCellValue('B6', $sixteen);
+
+                $sheet->setCellValue('C3', $thirteenFinish);
+                $sheet->setCellValue('C4', $fourteenFinish);
+                $sheet->setCellValue('C5', $fifteenFinish);
+                $sheet->setCellValue('C6', $sixteenFinish);
+
+                $sheet->setCellValue('F2', $registers);
+                $sheet->setCellValue('F3', $finish);
+
+
+                $groups = Group::whereHas('call', function ($query) {
+                    $query->whereRaw(' convocatoria = 2016  and id_grupos_musica > 31 ');
+                })->with(['call' => function ($q) {
+                    $q->whereRaw(' convocatoria = 2016  and id_grupos_musica > 31 ');
+                }])->orderBy('id', 'desc')->get();
+
+                $i = 9;
+                $sheet->appendRow(8,
+                    ['id', 'Nombre', 'Año inscripción', 'Fecha de inscripción', 'Fecha de Finalizacion', 'Formulario'
+                    ]);
+                foreach ($groups as $group) {
+                    $sheet->appendRow($i,
+                        [$group->id, $group->name, $group->call->inscripcion_inicial
+                            , $group->date_human, $group->call->date_human, $group->call->step
+                        ]);
+                    $i++;
+                }
+
 
             });
         })->export('xls');
@@ -58,9 +114,9 @@ class ReportController extends Controller
         $groups = Group::whereHas('call', function ($query) {
             $query->whereRaw('convocatoria = 2016');
 
-        })->where('id', '>' ,'31    ')->with('call')->get();
+        })->where('id', '>', '31    ')->with('call')->get();
 
-        $registers = Group::where('id', '>' ,'31')->count();
+        $registers = Group::where('id', '>', '31')->count();
         $finish = Call::whereRaw('convocatoria = 2016 and fecha_finalizacion IS NOT NULL ')->count();
         $thirteen = Call::whereRaw('convocatoria = 2016 and inscripcion_inicial = 2013 ')->count();
         $fourteen = Call::whereRaw('convocatoria = 2016 and inscripcion_inicial = 2014 ')->count();
@@ -72,8 +128,8 @@ class ReportController extends Controller
         $fifteenFinish = Call::whereRaw('convocatoria = 2016 and inscripcion_inicial = 2015 and fecha_finalizacion IS NOT NULL')->count();
         $sixteenFinish = Call::whereRaw('convocatoria = 2016 and inscripcion_inicial = 2016 and fecha_finalizacion IS NOT NULL')->count();
 
-        return view('reportUsers', compact('groups', 'step','registers','finish',
-            'thirteen','fourteen','fifteen','sixteen', 'thirteenFinish','fourteenFinish','fifteenFinish','sixteenFinish'));
+        return view('reportUsers', compact('groups', 'step', 'registers', 'finish',
+            'thirteen', 'fourteen', 'fifteen', 'sixteen', 'thirteenFinish', 'fourteenFinish', 'fifteenFinish', 'sixteenFinish'));
     }
 
     public function user($id)
--- resources/views/reportUsers.blade.php
+++ resources/views/reportUsers.blade.php
@@ -19,49 +19,67 @@
 
         <div class="row">
             <div class="col-4">
-            <table >
-                <thead>
-                <tr>
-                    <th>Año</th>
-                    <th>Inscritos</th>
-                    <th>Finalizados</th>
-                </tr>
-                </thead>
-                <tbody>
-                <tr>
-                    <td class="red">2013</td>
-                    <td>{{$thirteen}}</td>
-                    <td>{{$thirteenFinish}}</td>
-                </tr>
-                <tr>
-                    <td class="red">2014</td>
-                    <td>{{$fourteen}}</td>
-                    <td>{{$fourteenFinish}}</td>
-                </tr>
-                <tr>
-                    <td class="red">2015</td>
-                    <td>{{$fifteen}}</td>
-                    <td>{{$fifteenFinish}}</td>
-                </tr>
-                <tr>
-                    <td class="red">2016</td>
-                    <td>{{$sixteen}}</td>
-                    <td>{{$sixteenFinish}}</td>
-                </tr>
-                </tbody>
-            </table>
+                <table>
+                    <thead>
+                    <tr>
+                        <th>Año</th>
+                        <th>Inscritos</th>
+                        <th>Finalizados</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <td class="red">2013</td>
+                        <td>{{$thirteen}}</td>
+                        <td>{{$thirteenFinish}}</td>
+                    </tr>
+                    <tr>
+                        <td class="red">2014</td>
+                        <td>{{$fourteen}}</td>
+                        <td>{{$fourteenFinish}}</td>
+                    </tr>
+                    <tr>
+                        <td class="red">2015</td>
+                        <td>{{$fifteen}}</td>
+                        <td>{{$fifteenFinish}}</td>
+                    </tr>
+                    <tr>
+                        <td class="red">2016</td>
+                        <td>{{$sixteen}}</td>
+                        <td>{{$sixteenFinish}}</td>
+                    </tr>
+                    </tbody>
+                </table>
             </div>
-            <table class="col-5">
-                <tr>
-                    <td class="red">Inscritos</td>
-                    <td>{{$registers}}</td>
-                </tr>
-                <tr>
-                    <td class="red">Inscritos finalizados</td>
-                    <td>{{$finish}}</td>
-                </tr>
-            </table>
-
+            <div class="col-7">
+                <table>
+                    <tr>
+                        <td class="red">Inscritos</td>
+                        <td>{{$registers}}</td>
+                    </tr>
+                    <tr>
+                        <td class="red">Inscritos finalizados</td>
+                        <td>{{$finish}}</td>
+                    </tr>
+                </table>
+            </div>
+            <div class="col-2 excel" style="max-width: 50px;margin: 30px 0;">
+                <a href="{{route('usersExcel')}}">
+                    <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1"
+                         x="0px" y="0px" viewBox="0 0 76.222 125" enable-background="new 0 0 76.222 100"
+                         xml:space="preserve"><g>
+                            <g>
+                                <g>
+                                    <g>
+                                        <path d="M50.339,0l-3.535,0.075H9.022C4.038,0.075,0,4.108,0,9.088v81.896C0,95.965,4.038,100,9.022,100h58.176      c4.982,0,9.02-4.035,9.02-9.017V29.982l0.004-3.535L50.339,0z M68.202,90.983c0,0.552-0.451,0.999-1.003,0.999H9.022      c-0.558,0-1.006-0.447-1.006-0.999V9.088c0-0.548,0.448-0.996,1.006-0.996h37.782v21.891h21.397V90.983z"/>
+                                    </g>
+                                </g>
+                            </g>
+                            <polygon
+                                    points="47.125,80.28 58.191,80.28 43.642,58.79 55.734,40.927 44.668,40.927 38.109,50.618 31.55,40.927 20.48,40.927    32.576,58.79 18.028,80.28 28.601,80.28 29.09,80.28 42.074,80.28 38.161,73.927 33.393,73.927 38.109,66.961  "/>
+                        </g></svg>
+                </a>
+            </div>
 
         </div>
 
